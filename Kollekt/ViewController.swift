b8bae842ac7db8fdc2812ce40166094e755a71b0
vik denic <vik.denic@gmail.com>
2016-08-14 00:38:12 -0500

    translated code but nonsatisfactory

--- Kollekt/ViewController.swift
+++ Kollekt/ViewController.swift
@@ -12,12 +12,13 @@ import ScaledVisibleCellsCollectionView
 class ViewController: UIViewController {
 
     @IBOutlet var collectionView: UICollectionView!
+    
+    let transformCellValue = CGAffineTransform(scaleX: 1.0, y: 0.80)
+    let animationSpeed = 0.2
+    var isFirstTimeTransform = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.setScaledDesginParam(scaledPattern: .horizontalCenter, maxScale: 1.1, minScale: 0.7, maxAlpha: 1.0, minAlpha: 0.5)
-        collectionView.delegate = self
-        collectionView.dataSource = self
     }
 
 }
@@ -26,6 +27,12 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+
+        if indexPath.row == 0 && isFirstTimeTransform == true {
+            isFirstTimeTransform = false
+        } else {
+            cell.transform = transformCellValue
+        }
 
         return cell
     }
@@ -39,7 +46,80 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
 // MARK: - UIScrollViewDelegate
 extension ViewController: UIScrollViewDelegate {
 
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        collectionView.scaledVisibleCells()
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+
+        let pageWidth = CGFloat(200 + 30) //cellWidth + interItemLineSpacing
+
+        let currentOffset = scrollView.contentOffset.x
+        let targetOffset = targetContentOffset.pointee.x
+        var newTargetOffset = CGFloat(0)
+
+        if targetOffset > currentOffset {
+            newTargetOffset = CGFloat(ceilf(Float((currentOffset / pageWidth) * pageWidth)))
+        } else {
+            newTargetOffset = CGFloat(ceilf(Float((currentOffset / pageWidth) * pageWidth)))
+        }
+
+        if newTargetOffset < 0 {
+            newTargetOffset = 0
+        } else if newTargetOffset > scrollView.contentSize.width {
+            newTargetOffset = scrollView.contentSize.width
+        }
+
+        targetContentOffset.pointee.x = currentOffset
+        scrollView.setContentOffset(CGPoint(x: newTargetOffset, y: 0), animated: true)
+
+        var index = Int(newTargetOffset / pageWidth)
+
+        if index == 0 { //If first index
+            var cell = collectionView.cellForItem(at: IndexPath(item: index, section: 0))
+
+            UIView.animate(withDuration: animationSpeed, animations: { 
+                cell?.transform = .identity
+            })
+            cell = collectionView.cellForItem(at: IndexPath(item: index + 1, section: 0))
+            UIView.animate(withDuration: animationSpeed, animations: {
+                cell?.transform = self.transformCellValue
+            })
+        } else {
+            var cell = collectionView.cellForItem(at: IndexPath(item: index, section: 0))
+            UIView.animate(withDuration: animationSpeed, animations: {
+                cell?.transform = .identity
+            })
+            index -= 1 //left
+            cell = collectionView.cellForItem(at: IndexPath(item: index, section: 0))
+            UIView.animate(withDuration: animationSpeed, animations: {
+                cell?.transform = self.transformCellValue
+            })
+
+            index += 1
+            index += 1
+            cell = collectionView.cellForItem(at: IndexPath(item: index, section: 0))
+            UIView.animate(withDuration: animationSpeed, animations: {
+                cell?.transform = self.transformCellValue
+            })
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
